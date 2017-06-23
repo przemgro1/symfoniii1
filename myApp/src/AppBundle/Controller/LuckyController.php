4f0f93df350e564747a8e80cb8e0f38d35095b5a
przemek <itmanager1@westwing.pl>
2017-06-23 07:31:45 +0200

    Test 1

--- myApp/src/AppBundle/Controller/LuckyController.php
+++ myApp/src/AppBundle/Controller/LuckyController.php
@@ -3,6 +3,8 @@
 // src/AppBundle/Controller/LuckyController.php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -30,12 +32,39 @@ class LuckyController extends Controller
         $number = mt_rand(10000, 100000);
 
 
+        $em = $this->get('doctrine')->getManager();
+
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(19.99);
+        $product->setDescription('Ergonomic and stylish!');
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+//        $em->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+//        $em->flush();
+
+
+        $product = $em->getRepository('AppBundle:Product')
+            ->find(11);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id 11'
+            );
+        }
+
+//        $logger = $this->container->get('logger');
+//        $logger->info(print_r($product,1));
+
 //        $encoders = array(new XmlEncoder(), new JsonEncoder());
 //        $normalizers = array(new ObjectNormalizer());
 
 //        $serializer = new Serializer($normalizers, $encoders);
 //
 //
+
 //        $person = new Person();
 //        $person->setAge(36);
 //        $person->setName('Prz');
@@ -54,15 +83,20 @@ class LuckyController extends Controller
 
 
         // Create the Transport
-        $transport = (new Swift_SmtpTransport('wydawnictwo-promedia.home.pl', 25))
+        $transport = (new \Swift_SmtpTransport('wydawnictwo-promedia.pl', 25))
             ->setUsername('hotelarz2@wydawnictwo-promedia.pl')
             ->setPassword('qweRTY321zzAz@')
         ;
+
+
+
 
 // Create the Mailer using your created Transport
         $mailer = new \Swift_Mailer($transport);
 
         $message = new \Swift_Message('Hello Email');
+
+        $imgUrl = $message->embed(\Swift_Image::fromPath('http://oms.westwingpl.pl/themes/store/images/logo2.png'));
 
         $message
         ->setFrom('hotelarz2@wydawnictwo-promedia.pl')
@@ -71,12 +105,18 @@ class LuckyController extends Controller
             $this->renderView(
             // app/Resources/views/Emails/registration.html.twig
                 'Emails/registration.html.twig',
-                array('name' => $name)
+                array(
+                    'name' => $name,
+                    'url' => $imgUrl
+                )
             ),
             'text/html'
         );
 
-        $mailer->send($message);
+//        $mailer->send($message);
+
+        $doctrine = $this->container->get('doctrine');
+
 
 
 
